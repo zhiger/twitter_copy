c17c0c2707f3f3009825c5683cb402d09d6887bc
Nurbolat Kusmagul <argnur@gmail.com>
2014-12-10 09:20:38 +0000

    add update

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ source 'https://rubygems.org'
 gem 'rails', '4.1.0'
 gem 'bootstrap-sass', '3.2.0.0'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 
 group :development do
   gem 'sqlite3', '1.3.8'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,75 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
   def new
     @user = User.new
   end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   
   def show
     @user = User.find(params[:id])
   end
   
   def create
-    @user = User.new(user_params)
-    
-    if @user.save
-      flash[:success] = "Регистрация прошла успешно!"
-      redirect_to @user
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
     else
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
   
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  # Before filters
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
